517300341809f029ab0d7927c64596ed4e24d02f
krrzss <sarkarkhushi2006@gmail.com>
2026-03-21 15:55:56 +0530

    just checked it out

--- src/lib/auth-context.tsx
+++ src/lib/auth-context.tsx
@@ -94,7 +94,7 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
     );
 
     return () => subscription.unsubscribe();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+     
   }, []);
 
   // Google OAuth
